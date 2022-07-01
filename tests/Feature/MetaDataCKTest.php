2c85ff6310175a21a0eb6a9f59bdab70affde99d
Gaspar Giménez <gaspar.gimenez8@gmail.com>
2022-06-30 21:02:35 -0300

    Se añadio el chequeo de meta datos MetaDataCKTest.php

---
+++ tests/Feature/MetaDataCKTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class MetaDataCK extends TestCase
+{
+    
+    public function test_encoding()
+    {
+        for($i=1;$i<=7;$i++){
+            $response = $this->get('/jugar'.'/'.$i);
+            $pagina = $response->getContent(); //El html
+            echo substr_count($pagina, '<meta charset="utf-8">');
+            $this->assertEquals(1,substr_count($pagina, '<meta charset="utf-8">'));
+            //print_r($pagina);
+            //$response->AssertStatus(200);
+        }
+    }
+    public function test_lang_es()
+    {
+        for($i=1;$i<=7;$i++){
+            $response = $this->get('/jugar'.'/'.$i);
+            $pagina = $response->getContent(); //El html
+            $this->assertEquals(1,substr_count($pagina, '<html lang="es">'));
+            //print_r($pagina);
+            //$response->AssertStatus(200);
+        }
+    }
+    
+}
